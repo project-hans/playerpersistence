520ccaa23e88fd79f24a1a74c7b73ed9b1a05bdb
Caesar <truecaesarlp@gmail.com>
2025-01-05 20:12:53 +0100

    removed ensure database exists

--- src/main/kotlin/sh/bims/playerpersistence/DatabaseManager.kt
+++ src/main/kotlin/sh/bims/playerpersistence/DatabaseManager.kt
@@ -1,8 +1,6 @@
 package sh.bims.playerpersistence
 
 import org.jetbrains.exposed.sql.Database
-import org.postgresql.util.PSQLException
-import java.sql.DriverManager
 object DatabaseManager {
     private lateinit var databaseInstance: Database
     private lateinit var serverNodeValue: String
@@ -10,20 +8,8 @@ object DatabaseManager {
     val database: Database
         get() = databaseInstance
 
-    val SERVER_NODE: String
-        get() = serverNodeValue
-
     fun initialize(url: String, user: String, password: String, serverNode: String) {
-        val dbName = extractDatabaseName(url)
-        var baseUrl = extractBaseUrl(url)
-
         PlayerPersistence.logger.info("Initializing DatabaseManager for server node: $serverNode")
-
-        if (!baseUrl.endsWith("/"))
-            baseUrl += "/"
-        
-        ensureDatabaseExists(baseUrl, dbName, user, password)
-        
         databaseInstance = Database.connect(
             url = url,
             driver = "org.postgresql.Driver",
@@ -32,27 +18,5 @@ object DatabaseManager {
         )
         serverNodeValue = serverNode
         PlayerPersistence.logger.info("Database connection established successfully for server node: $serverNode")
-    }
-
-    private fun ensureDatabaseExists(baseUrl: String, dbName: String, user: String, password: String) {
-        PlayerPersistence.logger.info("Checking if database '$dbName' exists...")
-
-        try {
-            DriverManager.getConnection(baseUrl, user, password).use { connection ->
-                val statement = connection.createStatement()
-                statement.execute("CREATE DATABASE $dbName IF NOT EXISTS ")
-            }
-        } catch (error: PSQLException) {
-            PlayerPersistence.logger.error("Failed to ensure database exists: ${error.message}", error)
-            throw error
-        }
-    }
-
-    private fun extractDatabaseName(url: String): String {
-        return url.substringAfterLast("/")
-    }
-
-    private fun extractBaseUrl(url: String): String {
-        return url.substringBeforeLast("/")
     }
 }
