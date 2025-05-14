ea81250e02cc89f7ed004828899e16abb8f18427
caesarakalaeii <truecaesarlp@gmail.com>
2025-05-14 21:35:43 +0200

    fix: updated to new mappings BREAKING CHANGES Indexing of Inventory in DB changed

--- src/main/kotlin/sh/bims/playerpersistence/Serialization.kt
+++ src/main/kotlin/sh/bims/playerpersistence/Serialization.kt
@@ -4,6 +4,7 @@ import com.google.gson.JsonArray
 import com.google.gson.JsonObject
 import com.google.gson.JsonParser
 import com.mojang.serialization.JsonOps
+import net.minecraft.inventory.Inventory
 import net.minecraft.item.ItemStack
 import net.minecraft.server.network.ServerPlayerEntity
 
@@ -11,19 +12,9 @@ object Serialization {
     fun serializeInventory(player: ServerPlayerEntity): String {
         val invArray = JsonArray()
 
-        // Serialize main inventory
-        player.inventory.main.forEachIndexed { index, itemStack ->
-            serializeStack(itemStack, index)?.let { invArray.add(it) }
-        }
-
         // Serialize armor slots
-        player.inventory.armor.forEachIndexed { index, itemStack ->
-            serializeStack(itemStack, index + 100)?.let { invArray.add(it) }
-        }
-
-        // Serialize off-hand slot
-        player.inventory.offHand.forEachIndexed { index, itemStack ->
-            serializeStack(itemStack, index + 150)?.let { invArray.add(it) }
+        for (i in 1..player.inventory.size()) {
+            serializeStack(player.inventory.getStack(i), i)?.let { invArray.add(it) }
         }
 
         return invArray.toString()
@@ -55,40 +46,27 @@ object Serialization {
     }
 
     fun deserializeInventory(player: ServerPlayerEntity, inventoryData: String) {
-        val jsonOps = JsonOps.INSTANCE
-        val jsonArray = JsonParser.parseString(inventoryData).asJsonArray
-
-        player.inventory.clear()
-
-        jsonArray.forEach { jsonElement ->
-            val jsonObject = jsonElement.asJsonObject
-            val slot = jsonObject.get("Slot").asInt
-            val itemJson = jsonObject.get("ItemStack")
-
-            val itemStack = ItemStack.CODEC.parse(jsonOps, itemJson).result().orElse(ItemStack.EMPTY)
-
-            when {
-                slot < 36 -> player.inventory.setStack(slot, itemStack) // Main inventory
-                slot in 100..149 -> player.inventory.armor[slot - 100] = itemStack // Armor slots
-                slot in 150..199 -> player.inventory.offHand[slot - 150] = itemStack // Off-hand slot
-            }
-        }
+        deserializeStacks(player.inventory, inventoryData)
     }
 
     fun deserializeEnderChest(player: ServerPlayerEntity, enderChestData: String) {
-        val jsonOps = JsonOps.INSTANCE
-        val jsonArray = JsonParser.parseString(enderChestData).asJsonArray
+        deserializeStacks(player.enderChestInventory, enderChestData)
+    }
 
-        player.enderChestInventory.clear()
+    fun deserializeStacks(inventory: Inventory, stackData: String) {
+        val jsonOps = JsonOps.INSTANCE
+        val jsonArray = JsonParser.parseString(stackData).asJsonArray
+
+        inventory.clear()
 
         jsonArray.forEach { jsonElement ->
             val jsonObject = jsonElement.asJsonObject
-            val slot = jsonObject.get("Slot").asInt
-            val itemJson = jsonObject.get("ItemStack")
+            val slot = jsonObject["Slot"].asInt
+            val itemJson = jsonObject["ItemStack"]
 
             val itemStack = ItemStack.CODEC.parse(jsonOps, itemJson).result().orElse(ItemStack.EMPTY)
 
-            player.enderChestInventory.setStack(slot, itemStack)
+            inventory.setStack(slot, itemStack)
         }
     }
 }
